c88e1afc069801aef490848fb7b446e373c861ff
pedroarissa <pedro.arissa@gmail.com>
2021-11-09 16:40:33 -0300

    Finalizando sistema

--- Cliente.js
+++ Cliente.js
@@ -1,10 +1,13 @@
 export class Cliente {
-    constructor(nome, _cpf,) {
+    constructor(nome, _cpf, senha) {
         this.nome = nome;
         this._cpf = _cpf;
+        this._senha = senha;
     }
 
     get cpf() {
         return this._cpf;
     }
+
+    
 }
--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./Contas/ContaCorrente.js";
 import {ContaPoupanca} from "./Contas/ContaPoupanca.js";
-import { ContaSalario } from "./Contas/ContaSalario.js";
+import {ContaSalario} from "./Contas/ContaSalario.js";
+import {Gerente} from "./Funcionarios/Gerente.js";
+import {Diretor} from "./Funcionarios/Diretor.js"
+import {SistemaAutenticacao} from "./Funcionarios/SistemaAutenticacao.js";
 
-const pedro = new Cliente("Pedro", "11122233300");
+const pedro = new Cliente("Pedro", "11122233300", 456);
 const contaPedro = new ContaCorrente(pedro, "1001");
+const salarioPedro = new ContaSalario(pedro);
 
 const camila = new Cliente("Camila", "44455566600");
 const contaCamila = new ContaPoupanca(0, camila, "1001");
 
-contaPedro.depositar(500);
-contaPedro.transferir(100, contaCamila);
-contaCamila.sacar(50);
 
-const salarioPedro = new ContaSalario(pedro)
+const diretor = new Diretor("Glauco", 11122233300, 10000);
+diretor.cadastrarSenha(123456);
 
+const gerente = new Gerente("Jobson", 44455566600, 5000);
+gerente.cadastrarSenha(123);
 
-salarioPedro.depositar(100);
-salarioPedro.sacar(10);
+const diretorLogado = SistemaAutenticacao.login(diretor, 123456);
+const gerenteLogado = SistemaAutenticacao.login(gerente, 123);
 
-console.log(salarioPedro);
+console.log(diretorLogado,gerenteLogado)
+const clienteLogado = SistemaAutenticacao.login(pedro, 456);
 
-
+console.log(clienteLogado)
 
 
 
